42f27ab2aaa0f5e4c67065cfa2115522bb85a884
Tessica Mac <jessicamac48@yahoo.com>
2023-03-07 21:19:22 -0500

    creating the handle for submit

--- DonationPal-Sprints/client/src/components/login/login.js
+++ DonationPal-Sprints/client/src/components/login/login.js
@@ -5,43 +5,39 @@ import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function MyForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // Handler function for the login form 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert( `You entered ${email} and ${password}` )
+    }
 
 
-//   const handleChange = (event) => {
-//     const name = event.target.name;
-//     const value = event.target.value;
-//     setInputs(values => ({...values, [name]: value}))
-//   }
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     alert(inputs);
-//   }
-
-  return (
-    <form method="post">
-      <label>Username
-      <input 
-        type="text" 
-        name="email" 
-        value={email} 
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      </label>
-      <label>Password
+    return (
+        <form method="post" onSubmit={handleSubmit}>
+        <label>Username
         <input 
-          type="password" 
-          name="password" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)}
+            type="text" 
+            name="email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}
         />
         </label>
-        <input className="button-8" type="submit" />
-        
-    </form>
-  )
+        <label>Password
+            <input 
+            type="password" 
+            name="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            </label>
+            <input className="button-8" type="submit" />
+            
+        </form>
+    )
 }
 
 export default MyForm;
